f83435607060accabaa456af1eed8e8158649f7d
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2024-11-08 11:39:43 -0800

    Added duration_of_meeting into input.json
    Also collected duration_of_meeting inside main

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import json
 
 # Parse JSON input from input.txt
 with open('input.json') as f:
-    data = json.load(f)        
+    json_data = json.load(f)
+
+# json_data is a list of two elements
+    # 1st element is the number of tests
+    # 2nd element is the data  
 
 # 'data' is a list of dictionaries, where each dictionary represents a person
     # data = [ {person1}, {person2}, ... ]
@@ -11,6 +15,8 @@ with open('input.json') as f:
     # person = { 'schedule': [ [busy_slot1], [busy_slot2], ... ], 'daily_activity': [start, end] }
 
 # Extract data from JSON input
+number_of_tests = int(json_data[0])
+data = json_data[1]
 number_of_people = len(data)
 people = []     # Make a list to hold all people
 for i in range(number_of_people):
@@ -23,12 +29,15 @@ person1_DailyAct = people[0]['DailyAct']
 person2_schedule = people[1]['Schedule']
 person2_DailyAct = people[1]['DailyAct']
 
-duration_of_meeting = 30
+# Last element of data is always the duration of the meeting
+duration_of_meeting = int(data[-1])
 
 busy_slots = [person1_schedule, person2_schedule]
 working_period = [person1_DailyAct, person2_DailyAct]
 
+header = "Number of Tests: " + str(number_of_tests) + "\n"
 common_free_times = find_common_free_times(busy_slots, working_period, duration_of_meeting)
 
 with open("output.txt", "w") as f:
+    f.write(header)
     f.write(str(common_free_times))
