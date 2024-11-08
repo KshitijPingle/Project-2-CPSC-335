7f5ead5e17e09eb360218d7f0270afdb2844057c
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2024-11-08 12:11:48 -0800

    Added functionality to perform multiple tests

--- main.py
+++ main.py
@@ -6,42 +6,43 @@ def arrange_meeting(input_file, output_file) :
     with open(input_file) as f:
         json_data = json.load(f)
 
-    # json_data is a list of three elements
-        # 1st element is the number of tests
-        # 2nd element is the data
-        # 3rd element is the duration of the meeting
-    # 'data' is a list of dictionaries, where each dictionary represents a person
-        # data = [ {person1}, {person2}, ... ]
+    # 'json_data' is a list of all test cases
+    # 'data' is a list of test cases
+    # last element of 'data' is the duration of the meeting
+    # each 'data' object represents one test case
+        # data = [ {person1}, {person2}, ... , duration_of_meeting ]
     # Each person dictionary has the following keys
         # person = { 'schedule': [ [busy_slot1], [busy_slot2], ... ], 'daily_activity': [start, end] }
 
-    # Extract data from JSON input
-    number_of_tests = int(json_data[0])
-    data = json_data[1]
-    number_of_people = len(data)
-    people = []     # Make a list to hold all people
-    for i in range(number_of_people):
-        people.append(data[i])
+    output_str = "Number of Tests: " + str(len(json_data)) + "\n"
+    test_num = 1
 
-    # Doing this for now to test the json stuff
-    person1_schedule = people[0]['Schedule']
-    person1_DailyAct = people[0]['DailyAct']
+    # Run through all test cases
+    for test in json_data :
+        people = []
+        for i in range(len(test)):
+            people.append(test[i])
 
-    person2_schedule = people[1]['Schedule']
-    person2_DailyAct = people[1]['DailyAct']
+        # Doing this for now to test the json stuff
+        person1_schedule = people[0]['Schedule']
+        person1_DailyAct = people[0]['DailyAct']
 
-    # Last element of data is always the duration of the meeting
-    duration_of_meeting = int(data[-1])
+        person2_schedule = people[1]['Schedule']
+        person2_DailyAct = people[1]['DailyAct']
 
-    busy_slots = [person1_schedule, person2_schedule]
-    working_period = [person1_DailyAct, person2_DailyAct]
+        # Last element of 'test' is always the duration of the meeting
+        duration_of_meeting = int(test[-1])
 
-    header = "Number of Tests: " + str(number_of_tests) + "\n"
-    common_free_times = find_common_free_times(busy_slots, working_period, duration_of_meeting)
+        busy_slots = [person1_schedule, person2_schedule]
+        working_period = [person1_DailyAct, person2_DailyAct]
 
+        common_free_times = find_common_free_times(busy_slots, working_period, duration_of_meeting)
+        output_str += "\nTest Case " + str(test_num) + " Output:\n" + str(common_free_times) + "\n"
+        test_num += 1
+
+    
     with open(output_file, "w") as f:
-        f.write(header)
-        f.write(str(common_free_times))
+        f.write(output_str)
 # End of arrange_meeting() function
 
 
